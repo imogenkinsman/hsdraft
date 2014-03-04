3789c011f9c16869d660183eec4515108e0d613c
Jade McGough <jade@thezets.com>
2014-03-03 21:19:51 -0800

    finished Selection tests work - all passing now

---
+++ spec/factories/selections.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :selection do
+    chosen_card_id 1
+    second_card_id 2
+    third_card_id 3
+  end
+end
--- spec/models/selection_spec.rb
+++ spec/models/selection_spec.rb
@@ -13,10 +13,4 @@ describe Selection do
     expect(build(:selection)).to be_valid
   end
 
-  it "is invalid if a card in the set isn't 'picked'" do
-    expect(build(:selection, picked: nil)).not_to be_valid
-    expect(build(:selection, picked: 1)).to be_valid
-    expect(build(:selection, picked: 4)).not_to be_valid
-  end
-
 end
