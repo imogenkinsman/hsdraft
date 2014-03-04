6bcd84668645d5e9b07bd6ec85b33abdd4ea923a
Jade McGough <jade@thezets.com>
2014-03-03 18:13:55 -0800

    added failing tests for Selection

--- spec/models/selection_spec.rb
+++ spec/models/selection_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Selection do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_many(:comments) }
+
+  it 'can be instantiated' do
+    selection = Selection.new
+    expect(selection).to be_valid
+  end
+
+  it 'has a valid factory' do
+    expect(build(:selection)).to be_valid
+  end
+
+  it "is invalid if a card in the set isn't 'picked'" do
+    expect(build(:selection, picked: nil)).to be invalid
+    expect(build(:selection, picked: 1)).to be valid
+    expect(build(:selection, picked: 4)).to be invalid
+  end
+
 end
