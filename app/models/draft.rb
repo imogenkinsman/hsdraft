9015e3fa37d796a8af104700fa7e9f142bbda2e8
Jade McGough <jade@thezets.com>
2014-03-03 21:13:25 -0800

    add champion validator

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,4 +1,8 @@
 class Draft < ActiveRecord::Base
   has_many :selections
   has_many :comments
+
+  validates :champion, inclusion: { in: %w(mage priest warlock paladin rogue druid hunter shaman warrior),
+                                    message: "%w{value} is not a valid champion type"}
+
 end
