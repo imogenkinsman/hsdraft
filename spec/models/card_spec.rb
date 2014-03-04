a86abb92bf1f800e4bb739f89ad474e7722347b0
Jade McGough <jade@thezets.com>
2014-03-03 18:19:59 -0800

    added failing tests for Card model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Card do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'can be instantiated' do
+    card = Card.new
+    expect(card).to be_valid
+  end
+
+  it 'has a valid factory' do
+    expect(build(:card)).to be_valid
+  end
+
+  it "is invalid without a correct card type" do
+    expect(build(:card, type: 'minion')).to be_valid
+    expect(build(:card, type: 'spell')).to be_valid
+    expect(build(:card, type: 'weapon')).to be_valid
+    expect(build(:card, type: 'furniture')).not_to be_valid
+  end
+
 end
--- spec/models/selection_spec.rb
+++ spec/models/selection_spec.rb
@@ -14,9 +14,9 @@ describe Selection do
   end
 
   it "is invalid if a card in the set isn't 'picked'" do
-    expect(build(:selection, picked: nil)).to be invalid
-    expect(build(:selection, picked: 1)).to be valid
-    expect(build(:selection, picked: 4)).to be invalid
+    expect(build(:selection, picked: nil)).not_to be_valid
+    expect(build(:selection, picked: 1)).to be_valid
+    expect(build(:selection, picked: 4)).not_to be_valid
   end
 
 end
