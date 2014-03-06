3253e083c78fdecf3ef1b5a14a0e91532d07521a
Jade McGough <jade@thezets.com>
2014-03-06 04:14:44 -0800

    fix for specifying type when creating new Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,7 @@ class Card < ActiveRecord::Base
   validates :type, inclusion: { in: %w(minion spell weapon),
             message: "%{value} is not a valid card type" }
 
+  # in the future, don't name a column 'type'
+  self.inheritance_column = nil
+
 end
