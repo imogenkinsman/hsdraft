d8aaf3646689782ab35d9df7bd8c0044f5df683b
Jade McGough <jade@thezets.com>
2014-03-12 03:59:20 -0700

    fix missing js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require underscore
-//= require backbone
-//= require backbone/hsdraft
+//= require underscore-min
+//= require backbone-min
 //= require bootstrap
