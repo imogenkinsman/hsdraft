c15d92b9436298487190032084627424b94e3471
Jade McGough <jade@thezets.com>
2014-03-12 03:55:14 -0700

    add cards controller test

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe CardsController do
 
+  describe 'GET #index' do
+
+    it 'populates an array of cards' do
+      card = create(:card)
+      get :index, format: :json
+
+      expect(response).to be_success
+      expect(assigns(:cards)).to eq([card])
+    end
+
+  end
+
 end
