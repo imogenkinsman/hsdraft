ce243e2dc0d248e05aa79c81215e6c72b6f8ffe8
Jade McGough <jade@thezets.com>
2014-03-17 07:12:29 -0700

    load card data via ajax

--- app/assets/javascripts/drafts.js
+++ app/assets/javascripts/drafts.js
@@ -1,24 +1,33 @@
 $(function() {
 
-  var cards = ['sdfddf', 'asdf', 'assdf']
-
-  var engine = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: $.map(cards, function(card) { return { value: card }; })
+  var cards = [];
+  $.get('/cards.json', function(data) {
+    $.each(data, function(_, item) {
+      cards.push(item.card.name);
+    });
+    setup();
   });
 
-  engine.initialize();
+  function setup() {
+    var engine = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: $.map(cards, function(card) { return { value: card }; })
+    });
 
-  $('#card-search').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1
-  },
-  {
-    name: 'cards',
-    displayKey: 'value',
-    source: engine.ttAdapter()
-  });
+    engine.initialize();
+
+    $('#card-search').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+      },
+      {
+        name: 'cards',
+        displayKey: 'value',
+        source: engine.ttAdapter()
+      });
+  }
+
 });
 
