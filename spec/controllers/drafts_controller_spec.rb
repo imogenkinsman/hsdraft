5c4c888a9aa0febaeeb685fbac1a1dc7439b64ac
Jade McGough <jade@thezets.com>
2014-03-19 05:39:46 -0700

    adding drafts tests

--- spec/controllers/drafts_controller_spec.rb
+++ spec/controllers/drafts_controller_spec.rb
@@ -2,4 +2,44 @@ require 'spec_helper'
 
 describe DraftsController do
 
+  describe 'GET #new' do
+    it 'assigns a new Draft to @draft' do
+
+    end
+
+    it 'renders the :new template' do
+
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested Draft to @draft' do
+      draft = create(:draft)
+      get :show, id: draft
+      expect(assigns(:draft)).to eq(draft)
+    end
+
+    it 'renders the :show template' do
+
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new draft to the database' do
+
+      end
+
+      it 'redirects to #show' do
+
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the draft to the database' do
+
+      end
+    end
+  end
+
 end
