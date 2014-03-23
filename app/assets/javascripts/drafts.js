1be032e1b7e2daed6bc1efb3a801ea6393d6e82a
Jade McGough <jade@thezets.com>
2014-03-23 06:04:32 -0700

    migrating drafts code to one file

--- app/assets/javascripts/drafts.js
+++ app/assets/javascripts/drafts.js
@@ -1,41 +1,99 @@
+// backbone js for selection model on drafts page
+
 $(function() {
 
-  var availableCards;
-  var cardsJson;
-  var engine;
+  var Card = Backbone.Model.extend({
 
-  $.get('/cards.json', function(data) {
-    var currentHero = $('#card-search').data('hero');
-
-    var classCards = $.grep(data, function(element) {
-      return element.card.hero_id === currentHero || !element.card.hero_id
-    });
-
-    cardsJson = availableCards = classCards;
-    startTypeahead();
   });
 
-  // Configures and starts typeahead - run after ajax populates cards array
-  function startTypeahead() {
-    engine = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      local: $.map(availableCards, function(element) { return { value: element.card.name }; })
-    });
+  var Set = Backbone.Collection.extend({
+    model: Card
+  });
 
-    engine.initialize();
+  var CardView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'card',
+    initialize: function() {
+      _.bindAll(this, 'render');
+    },
 
-    $('#card-search').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-      },
-      {
-        name: 'cards',
-        displayKey: 'value',
-        source: engine.ttAdapter()
+    render: function() {
+      $(this.el).html('Test')
+      return this;
+    }
+  });
+
+  var SetView = Backbone.View.extend({
+
+    el: $('body'),
+
+    events: {
+      'typeahead:selected' : 'selected'
+    },
+
+    initialize: function() {
+      _.bindAll(this, 'selected', 'appendItem', 'startTypeahead');
+
+      this.collection = new Set();
+      this.collection.bind('add', this.appendItem);
+
+      this.count = 0;
+
+      var set = this;
+
+      $.get('/cards.json', function(data) {
+        var currentHero = $('#card-search').data('hero');
+
+        var classCards = $.grep(data, function(element) {
+          return element.card.hero_id === currentHero || !element.card.hero_id
+        });
+
+        set.cardsJson = set.availableCards = classCards;
+        set.startTypeahead();
       });
-  }
+
+    },
+
+    selected: function(e) {
+      if (this.count < 3) {
+        $('#card-search').val('');
+        var card = new Card();
+        this.collection.add(card);
+      }
+    },
+
+    appendItem: function(item) {
+      this.count++;
+      var cardView = new CardView({
+        model: item
+      });
+      $('#set').append(cardView.render().el);
+    },
+
+    // Configures and starts typeahead - run after ajax populates cards array
+    startTypeahead: function() {
+      engine = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: $.map(this.availableCards, function(element) { return { value: element.card.name }; })
+      });
+
+      engine.initialize();
+
+      $('#card-search').typeahead({
+          hint: true,
+          highlight: true,
+          minLength: 1
+        },
+        {
+          name: 'cards',
+          displayKey: 'value',
+          source: engine.ttAdapter()
+        });
+    }
+
+  });
+
+  var setView = new SetView();
 
 });
-
